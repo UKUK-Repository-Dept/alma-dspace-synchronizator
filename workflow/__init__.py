5bcb6a5f6771290a1003e144cb3ac9692bd97da0
Jakub Řihák <jakub.rihak@gmail.com>
2022-04-13 09:45:09 +0200

    Abstract class defining Workflow

--- workflow/__init__.py
+++ workflow/__init__.py
@@ -1,17 +1,25 @@
 from abc import ABC, abstractmethod
 import typing
 if typing:
-    import dspace_api, dspace_solr, configparser
+    import dspace_api, dspace_solr, configparser, argparse
 
 class Workflow(ABC):
     """
     Operations permissible in the worfow
     """
 
-    def __init__(self, dsapi: dspace_api, solr: dspace_solr, config: configparser.ConfigParser) -> None:
+    def __init__(self, dsapi: dspace_api, solr: dspace_solr, config: configparser.ConfigParser, args: argparse) -> None:
         self.__dsapi = dsapi
         self.__dspace_solr = solr
         self.__config = config
+        self.__args = args
+        self.__gathered_docs = list()
+        self.__mapfile_csv_list = list()
+        self.__mapfile_csv_fieldnames = list()
+
+    @property
+    def args(self) -> argparse:
+        return self.__args
 
     @property
     def config(self) -> configparser.ConfigParser:
@@ -23,22 +31,50 @@ class Workflow(ABC):
 
     @property
     def solr(self) -> dspace_solr:
-        return self.__dspace_solr
+        return self.__dspace_solr.solr
+
+    @property
+    def gathered_docs(self) -> list:
+        return self.__gathered_docs
+
+    @gathered_docs.setter
+    def gathered_docs(self, value):
+        self.__gathered_docs = value
+
+    @property
+    def mapfile_csv_list(self) -> list:
+        return self.__mapfile_csv_list
+
+    @mapfile_csv_list.setter
+    def mapfile_csv_list(self, value):
+        self.__mapfile_csv_list = value
+
+    @property
+    def mapfile_csv_fieldnames(self):
+        return self.__mapfile_csv_fieldnames
+
+    @mapfile_csv_fieldnames.setter
+    def mapfile_csv_fieldnames(self, value):
+        self.__mapfile_csv_fieldnames = value
 
     @abstractmethod
     def find_input_file(self):
         pass
 
     @abstractmethod
-    def find_valid_docs_in_solr(self):
+    def gather_valid_docs_in_solr(self):
         pass
 
     @abstractmethod
-    def find_valid_docs_in_mapfile(self):
+    def preprocess_solr_docs(self):
         pass
     
     @abstractmethod
     def compare_solr_to_mapfile(self):
+        pass
+
+    @abstractmethod
+    def prepare_record_updates(self):
         pass
 
     @abstractmethod
