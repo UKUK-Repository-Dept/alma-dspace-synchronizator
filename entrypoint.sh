69e37b993813ee02965024c460d4c0bf220f283a
Jakub Řihák <jakub.rihak@gmail.com>
2022-04-22 17:43:22 +0200

    Added entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Starting sysno-alma-dspace-sychronizator"
+
+echo "Processing params: MODE=$MODE LIMIT=$LIMIT"
+
+if $LIMIT == '' # empty string
+  then
+    echo "LIMIT is empty string, there is no limit."
+    echo "Starting processing in MODE $MODE without a limit."
+    python alma-dspace-synchronizer.py -m $MODE
+  else
+    echo "Starting processing in MODE=$MODE with LIMIT = $LIMIT"
+    python alma-dspace-synchronizer.py -m $MODE -l $LIMIT
+fi
